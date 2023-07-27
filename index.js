51484d9f9864d58b28bbe59108e6d157ac0471a0
Laleti Cynthia <+>
2023-07-27 16:58:33 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+//function declaration
+function saturdayFun(activity = "roller-skate") {
+       return `This Saturday, I want to ${activity}!`;
+    }
+saturdayFun("bathe my dog");
+//function expression
+let mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective (visualflair = "*"){
+    return function (adjective = "special"){
+        const highlight = "*"
+        return `You are ${visualflair}${adjective}${visualflair}!`;
+    };
+}
+const asteric = wrapAdjective("*");
+const slash = wrapAdjective("||");
